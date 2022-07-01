6ff44aba0996e10e62d4b64500e1027caef61343
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-01 12:14:34 +0100

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,6 +5,7 @@
 /**
  * main-> assign a random number to the variable n each time it is executed 
  * and prints the last didgit of the number stored in the variable n
+ * 
  * Return : always 0
  */
 int main(void)
