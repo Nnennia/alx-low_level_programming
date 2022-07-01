806812783f1e29b1119c6900ce725721dee57ab1
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-01 12:33:03 +0100

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * Program that prints the alphabet in lowercase followed by a new line
- * 
- * Return : always 0
- * 
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always (Success)
  */
-int main (void)
+int main(void)
 {
-char ch;
-for(ch = 'a'; ch <= 'z'; ch++);
-    {
-        putchar(ch);
-    }
-putchar("\n");
+	char c;
 
-return (0);
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
 }
