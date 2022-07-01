f4fb4d9dbe75b648fef96454cc579ce4e9e99a94
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-01 12:02:54 +0100

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * Program that prints the alphabet in lowercase followed by a new line
+ * Return : always 0
+ */
+int main (void)
+{
+    int ch;
+    for(ch = 97; ch <= 122; ch++);
+    {
+        putchar(ch);
+    }
+    putchar(10);
+
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Programme that prints the alphabet in lowercase
+ * followe by a new line
+ * Return : always 0
+ */
+int main(void)
+{
+    char ch;
+    for (ch = 'a'; ch <= 'z'; ch++);
+    {
+        if (ch != 'q' && ch != 'e');
+    }
+    {
+        putchar(ch);
+    }
+    putchar (10);
+
+    return (0);
+}
