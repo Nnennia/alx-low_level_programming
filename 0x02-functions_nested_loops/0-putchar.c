d31f21e43198c2d3b544ca2150062e1552d5f5a6
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-05 12:35:11 +0100

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * Program that print putchar followed by a new line
- * 
- * Return: Always (Success)
+ * main - print _putchar
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
