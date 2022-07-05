3bca4a82019bd9715fc5fff279ea0c4be8390f8c
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-05 10:36:46 +0100

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function that adds two integers and returns the result
+ * @r: int to be added to t
+ * @t: int to be added to r
+ * Return: sum of t and r
+ */
+int add( int r, int t)
+{
+    int b;
+
+    b = r + t;
+
+    return (b);
+}
