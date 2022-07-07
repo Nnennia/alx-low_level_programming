11537d11cb880d1a430a1d32572399b414cd493c
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-07 12:24:38 +0100

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,20 +2,19 @@
 #include <stdio.h>
 
 /**
- * void print_most_numbers - prints the numbers from 0-9
- * 
- * Return: always 0
+ * print_most_numbers - return  0-9 skips 2 and 4
  */
+
 void print_most_numbers(void)
 {
-    int b;
+	int b;
 
-    for (b = '0'; b <= '9'; b++)
-    {
-        if (b != '2' && b != '4')
-        {
-           _putchar(b);
-        }
-    }
-    _putchar('\n');
-}   
+	for (b = '0'; b <= '9'; b++)
+	{
+		if (b != '2' && b != '4')
+		{
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+}
