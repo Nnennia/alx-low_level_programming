f01a31d4be3eaa1525330d3de09bd0ac144f44e5
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-07 12:26:33 +0100

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
 
 /**
  * print_numbers - prints numbers from 0-9
- * 
- * Return: Always 0
  */
+
 void print_numbers(void)
 {
     int c;
 
-    for(c = '0'; c <= '9'; c++);
+    for(c = '0'; c <= '9'; c++)
     {
         _putchar(c);
     }
