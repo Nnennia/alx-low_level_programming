f8d991af0083dd510a9ef8ae76d7771f37c793f4
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-12 10:02:25 +0100

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,18 +1,35 @@
  #include "main.h"
-/**
- * _atoi - Function that convert strin to int
- * @arr: String to convert to int
- *
- * Return: Value of converted number
- */
-int _atoi(char *arr)
-{
-	int i = 0, convert = 0;
 
-	for (i = 0; arr[i] != '\0'; ++i)
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
 	{
-		convert = convert * 10 + arr[i] - '0';
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
 	}
 
-	return (convert);
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+	}
+	return (oi * pn);
 }
