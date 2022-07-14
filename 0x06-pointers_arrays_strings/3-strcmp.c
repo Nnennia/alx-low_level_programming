baeb63ff831e711637693be3b33b894b6bb250be
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-14 09:14:23 +0100

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,8 +10,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int result;
+	int i = 0;
 
-    result = strcmp(s1, s2);
-    return (result);
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
