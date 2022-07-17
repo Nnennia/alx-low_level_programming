4521370beacf94e423a7499bb041b4f8662ace4c
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-17 10:13:54 +0100

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strchr - searches a character in a string
+ * @s: The c string to be scanned
+ * @c: the character to be searched in str
+ * Return: pointer to the first occurance if c is found.
+ *         NULL if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int b;
+
+	for (b = 0; s[b] >= '\0'; b++)
+	{
+		if (s[b] == c)
+		    return (s + b);
+	}
+
+	return ('\0');
+}
