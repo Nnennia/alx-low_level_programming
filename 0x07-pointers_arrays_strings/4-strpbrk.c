2de5c278e09ec1e38135ad07e20870c852c7a842
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-18 12:28:13 +0100

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * strpbrk - locates a string for any set of bytes
- * @s: string to be scanned
- * @accept: containing the caharacters to match
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be scanned
+ * @accept: string containing the characters to match.
  *
- * Return: a pointer to the character if a set is matched
- *         else returns NULL
+ * Return: If a set is matched - a pointer to the matched byte.
+ *         If no set is matched - NULL.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int x;
 
-	while(*s)
+	while (*s)
 	{
-		for(x = 0; accept[x]; x++)
+		for (x = 0; accept[x]; x++)
 		{
-			if(*s == accept[x])
+			if (*s == accept[x])
 			    return (s);
 		}
 
