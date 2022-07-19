98cd336af2930ecb5e962ee9234e6b286b17e745
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-19 20:25:13 +0100

    commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_palindrome - empty string
+ * @s: string
+ * Return: 0 or 1
+ */
+int is_palindrome(char *s)
+{
+	if (*s == is_palindrome)
+		return (1);
+	else if (*s != is_palindrome)
+		return (0);
+}
