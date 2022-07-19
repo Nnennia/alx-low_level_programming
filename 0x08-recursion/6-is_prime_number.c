89abec2133aa28061dbf12831e764fd0843a37e6
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-19 20:06:58 +0100

    commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - checks prime number
+ * @n: int
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+/**
+ * checks_prime - checks all numbers less than n
+ * @n:int
+ * @resp:int
+ * Return: int
+ */
+int check_prime(int n, int resp)
+{
+	if (resp >= n && n > 1)
+	{
+		return (1);
+	}
+	else if (n % resp == 0 || n <= 1)
+	{
+		return (0);
+	}
+	else{
+		return (check_prime(n, resp + 1));
+	}
+}
