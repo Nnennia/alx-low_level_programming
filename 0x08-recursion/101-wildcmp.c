d38056856f279c73e918f211ee8febe07397e5e3
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-19 20:37:47 +0100

    commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * wildcmp - check the code for ALX School students.
+ * wildcmp - check the code for Holberton School students.
  * @s1: string
  * @s2: string
  * Return: Always 0.
@@ -10,7 +10,7 @@
 
 int wildcmp(char *s1, char *s2)
 {
-    return (checker(s1, s2, 0, 0, -1));
+	return (checker(s1, s2, 0, 0, -1));
 }
 
 /**
@@ -21,12 +21,12 @@ int wildcmp(char *s1, char *s2)
  */
 int checkLast(char *s, int i)
 {
-    if (s[i] == '*')
-	    return (checkLast(s, i + 1));
-    else if (s[i] == '\0')
-	    return (1);
+	if (s[i] == '*')
+		return (checkLast(s, i + 1));
+	else if (s[i] == '\0')
+		return (1);
 
-    return (0);
+	return (0);
 
 }
 /**
