89f9cc8f8f116e8687d9df1d42535a2007b52962
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-19 20:14:01 +0100

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@ int is_prime_number(int n)
 	return (check_prime(n, 2));
 }
 /**
- * checks_prime - checks all numbers less than n
+ * checks_prime - checks all numbers less than n, if it can be divided
  * @n:int
  * @resp:int
  * Return: int
@@ -18,14 +18,13 @@ int is_prime_number(int n)
 int check_prime(int n, int resp)
 {
 	if (resp >= n && n > 1)
-	{
 		return (1);
-	}
 	else if (n % resp == 0 || n <= 1)
 	{
 		return (0);
 	}
-	else{
+	else
+	{
 		return (check_prime(n, resp + 1));
 	}
 }
