f889375e354ca7c006cd8a9d05dc5a6c03970b97
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-22 13:24:54 +0100

    commit

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,16 +1,1 @@
- #include "main.h"
-#include <string.h>
-
-/**
- * *_strncpy - copies a string
- * @dest: pointer to the copied string
- * @src: pointer to the string to be copied
- * @n: int
- * Return: destination
- */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	strncpy(dest, src, n);
-	return (dest);
-}
+char *_strncpy(char *dest, char *src, int n) {}
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,1 @@
- #include "main.h"
-/**
- * _puts - prints a string
- * @str:  parameter
- *
- * Description: A function that prints a string
- * Return: void
- */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
+void _puts(char *s) {}
