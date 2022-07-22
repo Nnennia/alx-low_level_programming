efe103dc74ef7c73aff93bb3605d9a2e66750cf5
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-22 12:53:15 +0100

    commit

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,1 +1,16 @@
-char *_strncpy(char *dest, char *src, int n) {}
+ #include "main.h"
+#include <string.h>
+
+/**
+ * *_strncpy - copies a string
+ * @dest: pointer to the copied string
+ * @src: pointer to the string to be copied
+ * @n: int
+ * Return: destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,28 @@
-char *_strpbrk(char *s, char *accept) {}
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be scanned
+ * @accept: string containing the characters to match.
+ *
+ * Return: If a set is matched - a pointer to the matched byte.
+ *         If no set is matched - NULL.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+
+	while (*s)
+	{
+		for (x = 0; accept[x]; x++)
+		{
+			if (*s == accept[x])
+				return (s);
+		}
+
+		s++;
+	}
+
+	return ('\0');
+}
