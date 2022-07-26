b999f18621a78de75ea945ba6b1e7b0f30764cc1
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-26 05:25:02 +0100

    commit

---
+++ 0x0D-preprocesor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define FILE_NAME __FILE__
+
+/**
+ * main -  program that prints the name of the file it was compiled form
+ * Return: 0
+ */
+int main()
+{
+	printf("%s\n", FILE_NAME);
+	return (0);
+}
