cfc8d151a470a4abd10ff8aa0fa3a0781b40af2e
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-07-28 12:23:21 +0100

    commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+
+ int main()
+{
+    int num1, num2, mul;
+    printf("Enter two operands: ");
+    scanf("%d %d", &num1, &num2);
+    mul = num1 * num2;
+    for (num1 <= 10; num2 <= 10;)
+    {
+        printf("The produt is %d\n");
+    };
+    if (mul = num1 * num2);
+    else
+    {
+        printf("Error!");
+        return 98;
+    }
+}
