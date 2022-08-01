21899bc2fd0e6b711cce5e58f3085c586441c37a
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-08-01 12:09:37 +0100

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,7 +3,7 @@
 #define FILE_NAME __FILE__
 
 /**
- * main -  program that prints the name of the file it was compiled form
+ * main - program that prints the name of the file it was compiled form
  * Return: 0
  */
 int main()
