1da070ffbc13f20bc53fcf9818a9c61056b96aa7
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-08-03 12:14:00 +0100

    commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,24 @@
-#include "function_pointers.h"
+ #include "function_pointers.h"
 
 /**
  * int_index - searches for an integer
- * @array: array of intergers
+ * @array: array of integers
  * @size: size of array
  * @cmp: function pointer
- * Return: index of the first element for which the cmp function does not return 0
+ * Return: index of first element that matches with `cmp`, or -1 if none found
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
-
 	}
 
 	return (-1);
