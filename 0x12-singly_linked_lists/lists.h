0197f140f17574f6ab8cda023a7024a67c271d27
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-08-11 18:47:25 +0100

    commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
-#ifndef _LINKED_LIST_
-#define _LINKED_LIST_
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,12 +18,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 
 #endif
