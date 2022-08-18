9d33645a22ae7854111af4a6624bce2adc79617b
Nnennia <106754370+Nnennia@users.noreply.github.com>
2022-08-18 19:09:45 +0100

    commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 /**
- * @print_binary - fuunction that prints the binary representation of a number
+ * @print_binary - prints the binary representation of a number
  * @n: number to convert to binary
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
@@ -17,6 +19,6 @@ void print_binary(unsigned long int n)
 		putchar ('1');
 		return;
 	}
-	print_binary (n >> 1);
-	putchar ('0' + (n & 1));
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
